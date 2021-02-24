4de311f3e32a3cccf72627f03a7d3ee114a53dbe
Sayantan Dasgupta <absarka2001@gmail.com>
2021-02-24 13:55:36 +0530

    Login Signup pages modified

--- lib/main.dart
+++ lib/main.dart
@@ -19,14 +19,14 @@ class WelcomePage extends StatelessWidget {
               style: TextStyle(
                 fontWeight: FontWeight.bold,
                 color: Colors.black,
-                fontSize: 40
+                fontSize: 20
               ),
             ),
             SizedBox(height: 25,),
             FlatButton(
               color: Colors.green,
               child: Text("Login",
-                style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold,fontSize: 30),
+                style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold,fontSize: 20),
               ),
               onPressed: (){
                 Navigator.push(context, MaterialPageRoute(builder: (context)=>LoginPage()));
@@ -35,7 +35,7 @@ class WelcomePage extends StatelessWidget {
             FlatButton(
               color: Colors.green,
               child: Text("Sign Up",
-                style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold,fontSize: 30),
+                style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold,fontSize: 20),
               ),
               onPressed: (){
                 Navigator.push(context, MaterialPageRoute(builder: (context)=>SignupPage()));
