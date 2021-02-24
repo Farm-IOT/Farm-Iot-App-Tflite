2ff6b6f8df68ae22dd93dfe43b53c7527b2d31f4
Sayantan Dasgupta <absarka2001@gmail.com>
2021-02-24 12:03:58 +0530

    New Files added

--- lib/main.dart
+++ lib/main.dart
@@ -1,114 +1,48 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:tflite/tflite.dart';
+import 'package:machine_learning_flutter_app/screens/login.dart';
+import 'package:machine_learning_flutter_app/screens/signup.dart';
 
-void main() => runApp(MaterialApp(
-  home: MyApp(),
-));
-
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
+void main(){
+  runApp(MaterialApp(home: WelcomePage(),));
 }
 
-class _MyAppState extends State<MyApp> {
-  List _outputs;
-  File _image;
-  bool _loading = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _loading = true;
-
-    loadModel().then((value) {
-      setState(() {
-        _loading = false;
-      });
-    });
-  }
-
+class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.lightGreen,
-        title: const Text('Farm IOT - Pest Disease Classifier',
-        style: TextStyle(
-          fontWeight: FontWeight.bold,
-        ),
-        ),
-      ),
-      body: _loading
-          ? Container(
-        alignment: Alignment.center,
-        child: CircularProgressIndicator(),
-      )
-          : Container(
-        width: MediaQuery.of(context).size.width,
+      body: Center(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            _image == null ? Container() : Image.file(_image),
-            SizedBox(
-              height: 20,
-            ),
-            _outputs != null
-                ? Text(
-              "${_outputs[0]["label"]}",
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text("Welcome to Farm IoT App",
               style: TextStyle(
+                fontWeight: FontWeight.bold,
                 color: Colors.black,
-                fontSize: 20.0,
-                background: Paint()..color = Colors.white,
               ),
-            )
-                : Container()
+            ),
+            SizedBox(height: 20,),
+            FlatButton(
+              color: Colors.green,
+              child: Text("Login",
+                style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+              onPressed: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context)=>LoginPage()));
+              },
+            ),
+            FlatButton(
+              color: Colors.green,
+              child: Text("Sign Up",
+                style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+              onPressed: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context)=>SignupPage()));
+              },
+            ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: pickImage,
-        child: Icon(Icons.image),
-      ),
     );
-  }
-
-  pickImage() async {
-    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
-    if (image == null) return null;
-    setState(() {
-      _loading = true;
-      _image = image;
-    });
-    classifyImage(image);
-  }
-
-  classifyImage(File image) async {
-    var output = await Tflite.runModelOnImage(
-      path: image.path,
-      numResults: 2,
-      threshold: 0.5,
-      imageMean: 127.5,
-      imageStd: 127.5,
-    );
-    setState(() {
-      _loading = false;
-      _outputs = output;
-    });
-  }
-
-  loadModel() async {
-    await Tflite.loadModel(
-      model: "assets/model_unquant.tflite",
-      labels: "assets/labels.txt",
-    );
-  }
-
-  @override
-  void dispose() {
-    Tflite.close();
-    super.dispose();
   }
 }
---
+++ lib/screens/login.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:machine_learning_flutter_app/screens/image_analysis.dart';
+import 'package:machine_learning_flutter_app/screens/signup.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    double width=MediaQuery.of(context).size.width;
+    double height=MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: Container(
+        height: height,
+        width: width,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text('Login',style: TextStyle(fontSize: 25.0,fontWeight: FontWeight.bold),),
+                  ],
+                ),
+              ),
+              SizedBox(height: 30.0,),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Email',
+                  suffixIcon: Icon(Icons.email),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20.0,),
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                  suffixIcon: Icon(Icons.visibility_off),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                ),
+              ),
+              SizedBox(height: 30.0,),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Forget password?',style: TextStyle(fontSize: 12.0),),
+                    RaisedButton(
+                      child: Text('Login'),
+                      color: Color(0xffEE7B23),
+                      onPressed: (){
+                        Navigator.push(context, MaterialPageRoute(builder: (context)=>ImageAnalyse()));
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height:20.0),
+              GestureDetector(
+                onTap: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=>SignupPage()));
+                },
+                child: Text.rich(
+                  TextSpan(
+                      text: 'Don\'t have an account',
+                      children: [
+                        TextSpan(
+                          text: 'Signup',
+                          style: TextStyle(
+                              color: Color(0xffEE7B23)
+                          ),
+                        ),
+                      ]
+                  ),
+                ),
+              ),
+
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
